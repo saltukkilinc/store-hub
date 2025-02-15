ff34e0111b837ab5179ac8f879777615d5ae2b4a
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2025-02-15 13:10:45 +0300

    add sidebar submenu

---
+++ app/inventory/add/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AddInventoryPage() {
+  return (
+    <main className="container mx-auto p-8">
+      <h1>Add Inventory Form</h1>
+    </main>
+  );
+}
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,46 +1,113 @@
 import Link from "next/link";
+import Image from "next/image";
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 
 import {
   Sidebar,
   SidebarContent,
+  SidebarGroup,
   SidebarFooter,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubItem,
+  SidebarMenuSubButton,
 } from "@/components/ui/sidebar";
 
-import { Database, Store } from "lucide-react";
-const items = [
-  {
-    title: "Inventory",
-    url: "/inventory",
-    icon: Database,
-  },
-  {
-    title: "Ready For Sale Products",
-    url: "/ready-for-sale-products",
-    icon: Store,
-  },
-];
+import { Database, Minus, Plus, Store } from "lucide-react";
+
+const data = {
+  navMain: [
+    {
+      title: "Inventory",
+      url: "/inventory",
+      // icon: Database,
+      items: [
+        {
+          title: "Add Inventory",
+          url: "/inventory/add",
+          isActive: true,
+        },
+      ],
+    },
+    {
+      title: "Ready For Sale Products",
+      url: "/ready-for-sale-products",
+      // icon: Store,
+      items: [
+        {
+          title: "Add Product",
+          url: "/ready-for-sale-products/add",
+          isActive: false,
+        },
+      ],
+    },
+  ],
+};
 
 export default function AppSidebar() {
   return (
-    <Sidebar collapsible="icon">
-      <SidebarHeader />
-      <SidebarContent>
+    <Sidebar collapsible="offcanvas">
+      <SidebarHeader>
         <SidebarMenu>
-          {items.map((item) => (
-            <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton asChild>
-                <Link href={item.url}>
-                  <item.icon />
-                  <span>{item.title}</span>
-                </Link>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          ))}
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild size="lg">
+              <Link href={"/inventory"} className="flex items-center gap-2">
+                <Image src="/favicon.ico" alt="logo" width={36} height={36} />
+                <span>Store Hub</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
         </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            {data.navMain.map((item, index) => (
+              <Collapsible
+                key={item.title}
+                defaultOpen={index === 1}
+                className="group/collapsible"
+              >
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton>
+                      <Link href={item.url}>
+                        {/* <item.icon /> */}
+                        <span>{item.title}</span>
+                      </Link>
+                      <Plus className="ml-auto group-data-[state=open]/collapsible:hidden" />
+                      <Minus className="ml-auto group-data-[state=closed]/collapsible:hidden" />
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  {item.items?.length ? (
+                    <CollapsibleContent>
+                      <SidebarMenuSub>
+                        {item.items.map((item) => (
+                          <SidebarMenuSubItem key={item.title}>
+                            <SidebarMenuSubButton
+                              asChild
+                              isActive={item.isActive}
+                            >
+                              <a href={item.url}>{item.title}</a>
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                        ))}
+                      </SidebarMenuSub>
+                    </CollapsibleContent>
+                  ) : null}
+                </SidebarMenuItem>
+              </Collapsible>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
