002aee1bbf606f1f52f5ad1926539e108c5680d9
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2025-03-06 12:56:24 +0300

    add ready for sale product pie chart

--- app/visual-reports/page.tsx
+++ app/visual-reports/page.tsx
@@ -3,13 +3,16 @@ import PieChartInventoryProduct from "./pie-chart-inventory-product";
 import { getProducts } from "@/lib/actions/product-actions";
 import PieChartInventoryCategory from "./pie-chart-inventory-category";
 import { getCategories } from "@/lib/actions/category-actions";
+import PieChartReadyForSaleProduct from "./pie-chart-ready-for-sale-product";
+import { getReadyForSaleProducts } from "@/lib/actions/ready-for-sale-products-actions";
 
 // import ExampleChart from "./example-chart";
 
 export default async function VisualReportsPage() {
-  const inventory = await getInventory();
   const products = await getProducts();
   const categories = await getCategories();
+  const inventory = await getInventory();
+  const readyForSaleProducts = await getReadyForSaleProducts();
 
   return (
     <main className="container mx-auto p-8">
@@ -18,6 +21,10 @@ export default async function VisualReportsPage() {
       <section className="grid grid-cols-1 gap-3 sm:grid-cols-2 mt-4">
         <PieChartInventoryProduct data={inventory} products={products} />
         <PieChartInventoryCategory data={inventory} categories={categories} />
+        <PieChartReadyForSaleProduct
+          data={readyForSaleProducts}
+          products={products}
+        />
       </section>
     </main>
   );
