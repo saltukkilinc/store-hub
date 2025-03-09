adf3df97ec3bdd9ef8187ff93ac0a0bcbbb082a9
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2025-03-09 15:40:52 +0300

    refactor pie chart components

--- app/visual-reports/custom-pie-chart.tsx
+++ app/visual-reports/custom-pie-chart.tsx
@@ -17,38 +17,22 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart";
 
-import { ProductType } from "../products/product-data-table-column";
-import { getRandomHslColor } from "@/lib/utils";
-
-type PieChartProductPropsType<T extends { productName: string }> = {
+type CustomPieChartPropsType<T> = {
   data: T[];
-  products: ProductType[];
+  chartConfig: ChartConfig;
+  dataKey: string;
+  nameKey: string;
   title: string;
   description: string;
-  dataKey: string;
 };
-export default function PieChartInventoryProduct<
-  T extends { productName: string }
->({
+export default function CustomPieChart<T>({
   data,
-  products,
   title,
   description,
   dataKey,
-}: PieChartProductPropsType<T>) {
-  const chartConfig = products.reduce((acc: ChartConfig, product) => {
-    acc[product.productName.replace(" ", "")] = {
-      label: product.productName,
-      color: getRandomHslColor(),
-    };
-    return acc;
-  }, {});
-
-  const colorizedData = data.map((i) => ({
-    ...i,
-    fill: `var(--color-${i.productName.replace(" ", "")})`,
-  }));
-
+  nameKey,
+  chartConfig,
+}: CustomPieChartPropsType<T>) {
   return (
     <Card className="flex flex-col">
       <CardHeader className="items-center pb-0">
@@ -62,9 +46,9 @@ export default function PieChartInventoryProduct<
         >
           <PieChart>
             <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
-            <Pie data={colorizedData} dataKey={dataKey} nameKey="productName" />
+            <Pie data={data} dataKey={dataKey} nameKey={nameKey} />
             <ChartLegend
-              content={<ChartLegendContent nameKey="productName" />}
+              content={<ChartLegendContent nameKey={nameKey} />}
               className="-translate-y-2 flex-wrap gap-2 [&>*]:basis-1/4 [&>*]:justify-center"
             />
           </PieChart>
--- app/visual-reports/page.tsx
+++ app/visual-reports/page.tsx
@@ -1,14 +1,13 @@
-import PieChartProduct from "./pie-chart-product";
-import PieChartInventoryCategory from "./pie-chart-inventory-category";
-import PieChartReadyForSaleCategory from "./pie-chart-ready-for-sale-category";
+import CustomPieChart from "./custom-pie-chart";
 import BarChartProduct from "./bar-chart-inventory-stock-quantity";
 
 import { getReadyForSaleProducts } from "@/lib/actions/ready-for-sale-products-actions";
 import { getCategories } from "@/lib/actions/category-actions";
 import { getProducts } from "@/lib/actions/product-actions";
 import { getInventory } from "@/lib/actions/inventory-actions";
+import { ChartConfig } from "@/components/ui/chart";
+import { getRandomHslColor } from "@/lib/utils";
 
-// I didn't make a one big PieChart component because it increases complexity and reduces flexibility.
 export default async function VisualReportsPage() {
   const products = await getProducts();
   const categories = await getCategories();
@@ -26,20 +25,98 @@ export default async function VisualReportsPage() {
       color: "hsl(var(--chart-2))",
     },
   };
+  const productPieChartConfig = products?.reduce(
+    (acc: ChartConfig, product) => {
+      acc[product.productName.replace(" ", "")] = {
+        label: product.productName,
+        color: getRandomHslColor(),
+      };
+      return acc;
+    },
+    {}
+  );
+  const categoryPieChartConfig = categories.reduce((acc: ChartConfig, item) => {
+    acc[item.category.replace(/\s/g, "")] = {
+      label: item.category,
+      color: getRandomHslColor(),
+    };
+    return acc;
+  }, {});
+  const editedInventoryProductPieChartData = inventory.map((i) => ({
+    ...i,
+    fill: `var(--color-${i.productName.replace(" ", "")})`,
+  }));
+  const editedReadyForSaleProductsPieChartData = readyForSaleProducts.map(
+    (i) => ({
+      ...i,
+      fill: `var(--color-${i.productName.replace(" ", "")})`,
+    })
+  );
+  const handleEditInventoryCategoryPieChartData = () => {
+    const array: { category: string; fill: string; stockQuantity: number }[] =
+      [];
+    inventory.forEach((i) => {
+      const foundIndex = array.findIndex(
+        (item) => item.category === i.category
+      );
+      if (foundIndex === -1) {
+        array.push({
+          category: i.category,
+          stockQuantity: i.stockQuantity,
+          fill: `var(--color-${i.category.replace(/\s/g, "")})`,
+        });
+      }
+      if (foundIndex !== -1) {
+        array[foundIndex].stockQuantity += i.stockQuantity;
+      }
+    });
+    return array;
+  };
+  const handleEditReadyForSaleProductCategoryPieChart = () => {
+    const array: {
+      category: string;
+      fill: string;
+      availableQuantity: number;
+    }[] = [];
+    readyForSaleProducts.forEach((i) => {
+      const foundIndex = array.findIndex(
+        (item) => item.category === i.category
+      );
+      if (foundIndex === -1) {
+        array.push({
+          category: i.category,
+          availableQuantity: i.availableQuantity,
+          fill: `var(--color-${i.category.replace(/\s/g, "")})`,
+        });
+      }
+      if (foundIndex !== -1) {
+        array[foundIndex].availableQuantity += i.availableQuantity;
+      }
+    });
+    return array;
+  };
   return (
     <main className="container mx-auto p-8">
       <h1 className="text-2xl">Visual Reports</h1>
       {/* <ExampleChart /> */}
       <section className="grid grid-cols-1 gap-x-3 gap-y-8 sm:grid-cols-2 mt-12">
         <h2 className="text-xl sm:col-span-2">Inventory</h2>
-        <PieChartProduct
-          data={inventory}
-          products={products}
+        <CustomPieChart
+          data={editedInventoryProductPieChartData}
+          chartConfig={productPieChartConfig}
           title="Inventory Product Pie Chart"
           description="This chart shows the product distribution in inventory data."
           dataKey="stockQuantity"
+          nameKey="productName"
         />
-        <PieChartInventoryCategory data={inventory} categories={categories} />
+        <CustomPieChart
+          data={handleEditInventoryCategoryPieChartData()}
+          chartConfig={categoryPieChartConfig}
+          title="Inventory Category Pie Chart"
+          description="This chart shows the category distribution in inventory data."
+          dataKey="stockQuantity"
+          nameKey="category"
+        />
         <BarChartProduct
           data={inventory}
           xAxisDataKey="productName"
@@ -51,16 +128,23 @@ export default async function VisualReportsPage() {
           description="This bar chart shows the product stock quantity distribution in the inventory data."
         />
         <h2 className="text-xl sm:col-span-2">Ready For Sale Products</h2>
-        <PieChartProduct
-          data={readyForSaleProducts}
-          products={products}
+        <CustomPieChart
+          data={editedReadyForSaleProductsPieChartData}
+          chartConfig={productPieChartConfig}
           title="Ready For Sale Product Pie Chart"
           description="This chart shows the product distribution in Ready for sale products."
           dataKey="availableQuantity"
+          nameKey="productName"
         />
-        <PieChartReadyForSaleCategory
-          data={readyForSaleProducts}
-          categories={categories}
+
+        <CustomPieChart
+          data={handleEditReadyForSaleProductCategoryPieChart()}
+          chartConfig={categoryPieChartConfig}
+          title="Ready For Sale Category Pie Chart"
+          description="This chart shows the ready for sale category distribution in ready for sale
+          data."
+          dataKey="availableQuantity"
+          nameKey="category"
         />
         <BarChartProduct
           data={readyForSaleProducts}
